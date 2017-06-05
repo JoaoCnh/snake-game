d437d6a938fc79cb4c7fb8886031354c07515e49
JoaoCnh <joaomarinho.cunha@gmail.com>
2017-06-05 23:57:25 +0100

    Added Snake game

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# snake-game
-A plain Javascript snake game
+# Arkade
+A plain Javascript misc of games
--- assets/volcan/js/script.js
+++ assets/volcan/js/script.js
@@ -94,13 +94,3 @@ $(document).ready(function () {
 		});
 
 });
-
-/*--------------popup------------*/
-$('.open-popup-link').magnificPopup({
-  type:'inline',
-  midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-  gallery:{
-    enabled:true
-  }
-});
-
