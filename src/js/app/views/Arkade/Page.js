305cf8d40914e6d517f6e1beef7e82dde4a78193
João Cunha <joao.cunha@wiremaze.com>
2017-06-27 08:21:39 +0000

    Added page transitions :D

--- src/js/app/views/Arkade/Page.js
+++ src/js/app/views/Arkade/Page.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 import Home from 'app/views/Home';
 import Games from 'app/views/Games';
 import About from 'app/views/About';
 import NotFound from 'app/views/NotFound';
 
+const CSSTransitionRoute = ({ transitionName, ...props }) => (
+  <Route render={({ location }) => (
+    <CSSTransitionGroup transitionName={transitionName}
+      transitionEnterTimeout={300}
+      transitionLeaveTimeout={500}>
+      <Route location={location} key={location.key} {...props} />
+    </CSSTransitionGroup>
+  )} />
+);
+
 export default class Page extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/games" component={Games} />
-        <Route path="/about" component={About} />
+      <div>
+        <CSSTransitionRoute transitionName="fade" exact path="/" component={Home} />
+        <CSSTransitionRoute transitionName="fade" path="/games" component={Games} />
+        <CSSTransitionRoute transitionName="fade" path="/about" component={About} />
         <Route path="*" component={NotFound} />
-      </Switch>
+      </div>
     );
   }
 };
